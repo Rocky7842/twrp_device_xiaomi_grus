4507ee5de72d7ebacbbb90eedf57c1dab6bc1f97
me-cafebabe <me_cafebabe@hotmail.com>
2023-01-18 17:59:05 +0800

    grus: Support separated vendor tree

--- device.mk
+++ device.mk
@@ -13,6 +13,12 @@ MI710_KEYMASTER_VERSION := 4.0
 # Shipping API Level
 PRODUCT_SHIPPING_API_LEVEL := 28
 
+# Vendor blobs
+ifneq ($(wildcard vendor/xiaomi/grus/proprietary/),)
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,vendor/xiaomi/grus/proprietary/vendor/,$(TARGET_COPY_OUT_RECOVERY)/root/system/)
+endif
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
